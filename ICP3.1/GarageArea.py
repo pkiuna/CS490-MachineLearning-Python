5c2760319c49f469b1543e1b4c4175dce1676356
pkiuna <pkkygb@mail.umkc.edu>
2020-06-23 23:13:59 -0500

    ICP3.1

---
+++ ICP3.1/GarageArea.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+#emulating ggplot style sheet
+import matplotlib.pyplot as plt
+
+train = pd.read_csv('train.csv')
+
+train.SalePrice.describe()
+print(train[['GarageArea']], train[['SalePrice']])
+
+#scatter plot
+plt.style.use(style='ggplot')
+#changing rc settings in the matplotlib package
+plt.rcParams['figure.figsize'] = (15, 6)
+plt.scatter(train.GarageArea, train.SalePrice, color='g')
+
+fltr = train[(train.GarageArea < 1000) & (train.GarageArea > 300) & (train.SalePrice < 700000)]
+plt.scatter(fltr.GarageArea,fltr.SalePrice, color='g')
+plt.show()
+
+
+
+
+
+
+
+
+
+
