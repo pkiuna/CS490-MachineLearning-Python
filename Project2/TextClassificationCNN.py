2de60cf640970475bfbb93c6c9283fe55d3c587a
pkiuna <pkkygb@mail.umkc.edu>
2020-07-29 22:42:14 -0500

    Project 2

--- Project2/TextClassificationCNN.py
+++ Project2/TextClassificationCNN.py
@@ -20,7 +20,116 @@ from keras.utils.np_utils import to_categorical
 import re
 import matplotlib.pyplot as plt
 
-df = pd.read_csv()
+df = pd.read_csv("train.tsv.zip")
+df = df[['Phrase', 'Sentiment']]
+pd.set_option('display.max_colwidth', -2)
+df.head(4)
+
+df.Sentiment.value_counts()
+df_0 = df[df['Sentiment'] == 0].sample(frac=1)
+df_1 = df[df['Sentiment'] == 1].sample(frac=1)
+df_2 = df[df['Sentiment'] == 2].sample(frac=1)
+df_3 = df[df['Sentiment'] == 3].sample(frac=1)
+df_4 = df[df['Sentiment'] == 4].sample(frac=1)
+
+#balancing the set
+sample_size = 7072
+data = pd.concat([df_0.head(sample_size), df_1.head(sample_size), df_2.head(sample_size), df_3.head(sample_size), df_4.head(sample_size)]).sample(frac=1)
+
+#declare a vector size
+data['l'] = data['Phrase'].apply(lambda x: len(str(x).split(' ')))
+#using the sentence string as a whole
+sequence_length = 50
+
+max_features = 20000  #number of word to consider
+
+tokenizer = Tokenizer(num_words=max_features, split=' ', oov_token='<unw>')
+tokenizer.fit_on_texts(data['Phrase'].values)
+
+# replacing each word with an integer
+X = tokenizer.texts_to_sequences(data['Phrase'].values)
+
+#set sentences to equal length
+X = pad_sequences(X, sequence_length)
+#data for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+print("test set size " + str(len(X_test)))
+
+#embedding dimensions
+embedding_dim = 300
+num_filters = 100
+inputs = Input(shape=(sequence_length,), dtype='int32')
+
+#random embedding for text
+embedding_layer = Embedding(input_dim=max_features, output_dim=embedding_dim, input_length=sequence_length)(inputs)
+reshape = Reshape((sequence_length, embedding_dim, 1))(embedding_layer)
+
+conv_0 = Conv2D(num_filters, kernel_size=(3, embedding_dim), activation='relu', kernel_regularizer=regularizers.l2(3))(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(4, embedding_dim), activation='relu', kernel_regularizer=regularizers.l2(3))(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(5, embedding_dim), activation='relu', kernel_regularizer=regularizers.l2(3))(reshape)
+
+# perform max pooling on each of the convoluations
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - 3 + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - 4 + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - 5 + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+# concatenate and flatten
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+
+  # dropout and predict
+dropout = Dropout(0.5)(flatten)
+output = Dense(units=5, activation='softmax')(dropout)
+
+#using adam as the optimizer
+model = Model(inputs=inputs, outputs=output)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+batch_size = 40  #picking random batch size as the sample
+history = model.fit(X_train, y_train, epochs=20, batch_size=batch_size, verbose=1, validation_split=0.1, shuffle=True)
+
+#plot the training
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+
+#test against test set
+y_hat = model.predict(X_test)
+accuracy_score(list(map(lambda x: np.argmax(x), y_test)), list(map(lambda x: np.argmax(x), y_hat)))
+
+confusion_matrix(list(map(lambda x: np.argmax(x), y_test)), list(map(lambda x: np.argmax(x), y_hat)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
